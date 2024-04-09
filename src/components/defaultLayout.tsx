7afa2b9042425240d8f3cec3971d9bc4317c9e60
David Ishmael <dishmael3@gmail.com>
2024-04-09 14:18:57 -0400

    Added comments

--- src/components/defaultLayout.tsx
+++ src/components/defaultLayout.tsx
@@ -1,12 +1,6 @@
-"use client"
-
-import React from "react";
-
 /**
- * A lot of the UI component libraries require themes that cannot run on the
- * server. This DefaultLayout component acts as a wrapper to utilize those
- * client-side components without interferring with the standard layout code
- * that runs on the server.
+ * Custom layout wrapper so we can add contexts and themes without modifying the
+ * default layout code.
  * 
  * @param param0 
  * @returns 
