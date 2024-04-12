557afe017636e0994206820f1cf034c6429a2319
David Ishmael <dishmael3@gmail.com>
2024-04-12 09:39:26 -0400

    fixed layout

--- src/app/adminLayout.tsx
+++ src/app/adminLayout.tsx
@@ -14,9 +14,9 @@ export const metadata: Metadata = {
 
 /**
  * The admin layout includes a Header, Sidebar, and Main area
- * 
- * @param param0 
- * @returns 
+ *
+ * @param param0
+ * @returns
  */
 export default function RootLayout({
   children,
@@ -29,15 +29,29 @@ export default function RootLayout({
         className={`min-h-screen bg-background font-sans antialiased ${fontSans.variable}`}
       >
         <div className="flex flex-col h-screen overflow-hidden">
-          <div className="sticky top-0 z-50 bg-white shadow-md p-4">Header</div>
+          {/* Header Section */}
+          <div className="sticky top-0 z-50 border-b shadow-md p-4">Header</div>
+
+          {/* Sidebar and Main Sections */}
           <div className="flex flex-row flex-grow overflow-hidden">
-            <div className="sticky top-0 h-full w-48 overflow-y-auto bg-gray-100 p-4">
+            
+            {/* Sidebar */}
+            <div className="sticky top-0 h-full w-48 overflow-y-auto border-r shadow-md p-4">
               Sidebar
             </div>
+
+            {/* Main */}
             <div className="flex-grow overflow-x-auto overflow-y-auto p-4">
               {children}
             </div>
           </div>
+
+          {/* Footer Section */}
+          {/*
+          <div className="sticky bottom-0 z-50 border-t shadow-md p-4">
+            Footer
+          </div>
+          */}
         </div>
       </body>
     </html>
