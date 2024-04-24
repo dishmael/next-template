64d308910949208e45104f9b33914eb1205e838c
David Ishmael <dishmael3@gmail.com>
2024-04-24 17:21:39 -0400

    Added Avatar

--- src/components/nav/header.tsx
+++ src/components/nav/header.tsx
@@ -4,7 +4,9 @@ import React from "react";
 import { Brand } from "@/components/ui/brand";
 import { Menu } from "lucide-react";
 import { ModeToggle } from "@/components/ui/modeToggle";
-import { useAppContext } from "../contexts/appContext";
+import { useAppContext } from "@/components/contexts/appContext";
+import { Account } from "@/components/ui/account";
+import { Separator } from "../ui/separator";
 
 const Header = () => {
   const { setSidebarOpen } = useAppContext();
@@ -15,21 +17,23 @@ const Header = () => {
     <div>
       {/* Mobile View */}
       <div className="sm:hidden sticky top-0 z-50 border-b p-4 items-center bg-slate-50 dark:bg-inherit">
-        <div className="flex h-8 items-center space-x-4">
+        <div className="flex h-8 items-center space-x-2">
           <Menu size={20} onClick={handleClick} />
           <div className="flex-grow"></div>
           <ModeToggle />
-          <div>Mobile Header</div>
+          <Separator orientation="vertical" />
+          <Account />
         </div>
       </div>
 
       {/* Desktop View */}
       <div className="hidden sm:block sticky top-0 z-50 border-b p-4 bg-slate-50 dark:bg-inherit">
-        <div className="flex h-8 items-center space-x-4">
+        <div className="flex h-8 items-center space-x-2">
           <Brand />
           <div className="flex-grow"></div>
           <ModeToggle />
-          <div>Desktop Header</div>
+          <Separator orientation="vertical" />
+          <Account />
         </div>
       </div>
     </div>
---
+++ src/components/ui/account.tsx
@@ -0,0 +1,53 @@
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function Account() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative size-10 rounded-full">
+          <Avatar className="size-10">
+            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+            <AvatarFallback>DI</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1.5">
+            <p className="text-sm font-medium leading-none">David Ishmael</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              VP, Engineering
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          Log out
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
