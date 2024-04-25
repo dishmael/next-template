ef344c9cb02bb7b66ca05e699b299a1607b60cb6
David Ishmael <dishmael3@gmail.com>
2024-04-25 14:08:21 -0400

    layout change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,29 @@
-import { Button } from "@/components/ui/button";
-import { Repeater } from "@/components/ui/repeater";
-
-export default function Home() {
+export const Home = () => {
   return (
     <div className="flex flex-1 flex-col gap-2 p-4 lg:gap-4 lg:p-4 h-full">
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">Home</h1>
       </div>
-      <div
-        className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-        x-chunk="dashboard-02-chunk-1"
-      >
-        <div className="flex flex-col items-center gap-1 text-center">
-          <h3 className="text-2xl font-bold tracking-tight">
-            This is where content will go
-          </h3>
+      <div className="grid grid-rows-[196px,auto]  grid-cols-2 gap-6 h-full">
+        <div className="p-4 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-slate-900">
+          <h3 className="text-xl font-bold tracking-tight">Metric 1</h3>
           <p className="text-sm text-muted-foreground">
-            Once we start creating content, you can find it here.
+            The first set of metrics goes here.
           </p>
-          <Button className="mt-4">Home Button</Button>
+        </div>
+        <div className="p-4 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-slate-900">
+          <h3 className="text-xl font-bold tracking-tight">Metric 2</h3>
+          <p className="text-sm text-muted-foreground">
+            The second set of metrics goes here.
+          </p>
+        </div>
+        <div className="p-4 col-span-2 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-slate-900">
+          <h3 className="text-xl font-bold tracking-tight">Table 1</h3>
+          <p className="text-sm text-muted-foreground">A table goes here</p>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Home;
