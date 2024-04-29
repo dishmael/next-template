5b1c2ac50fb766cc3c11f05f9734e4ca2955091f
David Ishmael <dishmael3@gmail.com>
2024-04-29 09:58:48 -0400

    added hooks

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,16 @@
+"use client";
+
+import { Dispatch, SetStateAction, useState } from "react";
+
+export type ToggleState = Dispatch<SetStateAction<boolean>>;
+
+/**
+ * useState is a simple hook to manage a boolean state. By default, the state
+ * is 'false' but you can pass in 'true' as the initial state.
+ * 
+ * @param state the iniital state, default 'false'
+ * @returns
+ */
+const useToggle = (state?: boolean): (boolean | ToggleState)[] => {
+  return useState<boolean>(state || false);
+};
