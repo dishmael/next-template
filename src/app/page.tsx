a0c4d7815d591590cb991567ba604f0b1bcb8479
David Ishmael <dishmael3@gmail.com>
2024-04-29 08:29:10 -0400

    adjusted layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export const Home = () => {
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">Home</h1>
       </div>
-      <div className="grid grid-rows-[196px,auto]  grid-cols-2 gap-6 h-full">
+      <div className="grid sm:grid-rows-[196px,auto] grid=cols-1 sm:grid-cols-2 gap-6 h-full">
         <div className="p-4 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-slate-900">
           <h3 className="text-xl font-bold tracking-tight">Metric 1</h3>
           <p className="text-sm text-muted-foreground">
@@ -17,7 +17,7 @@ export const Home = () => {
             The second set of metrics goes here.
           </p>
         </div>
-        <div className="p-4 col-span-2 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-slate-900">
+        <div className="p-4 sm:col-span-2 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-slate-900">
           <h3 className="text-xl font-bold tracking-tight">Table 1</h3>
           <p className="text-sm text-muted-foreground">A table goes here</p>
         </div>
