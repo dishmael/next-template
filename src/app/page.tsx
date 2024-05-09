3f543edc28dcd1bf97c0a24d148eca42875d61e6
David Ishmael <dishmael3@gmail.com>
2024-05-09 15:53:14 -0400

    Tweaked layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,49 @@
-import { Repeater } from "@/components/ui/repeater";
+const Home = () => {
+  return (
+    <div className="flex flex-col p-4 gap-6 h-full">
+      {/* Row 1: Header */}
+      <div className="flex items-center">
+        <h1 className="text-lg font-semibold md:text-2xl">Home</h1>
+      </div>
 
-export default function Home() {
+      {/* Row 2: Metrics */}
+      <div className="flex grid-col-1 sm:grid-col-3 w-full gap-6 h-48">
+        <div className="flex-grow p-4 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-gray-800">
+          <h3 className="text-xl font-bold tracking-tight">Metric 1</h3>
+          <p className="text-sm text-muted-foreground">
+            The set of metrics goes here.
+          </p>
+        </div>
+        <div className="flex-grow p-4 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-gray-800">
+          <h3 className="text-xl font-bold tracking-tight">Metric 2</h3>
+          <p className="text-sm text-muted-foreground">
+            The set of metrics goes here.
+          </p>
+        </div>
+        <div className="flex-grow p-4 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-gray-800">
+          <h3 className="text-xl font-bold tracking-tight">Metric 3</h3>
+          <p className="text-sm text-muted-foreground">
+            The set of metrics goes here.
+          </p>
+        </div>
+        <div className="flex-grow p-4 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-gray-800">
+          <h3 className="text-xl font-bold tracking-tight">Metric 4</h3>
+          <p className="text-sm text-muted-foreground">
+            The set of metrics goes here.
+          </p>
+        </div>
+      </div>
+
+      {/* Row 3: Table */}
+      <div className="flex-grow p-4 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-gray-800">
+        <h3 className="text-xl font-bold tracking-tight">Table 1</h3>
+        <p className="text-sm text-muted-foreground">A table goes here</p>
+      </div>
+    </div>
+  );
+};
+
+const Home_Old = () => {
   return (
     <div className="flex flex-1 flex-col gap-2 p-4 lg:gap-4 lg:p-4 h-full">
       <div className="flex items-center">
@@ -19,6 +62,12 @@ export default function Home() {
             The second set of metrics goes here.
           </p>
         </div>
+        <div className="p-4 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-gray-800">
+          <h3 className="text-xl font-bold tracking-tight">Metric 3</h3>
+          <p className="text-sm text-muted-foreground">
+            The third set of metrics goes here.
+          </p>
+        </div>
         <div className="p-4 sm:col-span-2 rounded-lg border border-dashed shadow-sm bg-slate-50 dark:bg-gray-800">
           <h3 className="text-xl font-bold tracking-tight">Table 1</h3>
           <p className="text-sm text-muted-foreground">A table goes here</p>
@@ -26,4 +75,6 @@ export default function Home() {
       </div>
     </div>
   );
-}
+};
+
+export default Home;
