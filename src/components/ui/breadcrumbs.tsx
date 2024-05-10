b05b255480eb04186c6aef65690c42f2033f26a3
David Ishmael <dishmael3@gmail.com>
2024-05-10 14:25:49 -0400

    adjusted to make href optional

--- src/components/ui/breadcrumbs.tsx
+++ src/components/ui/breadcrumbs.tsx
@@ -3,26 +3,44 @@ import Link from "next/link";
 import { FC, ReactElement, ReactNode, Children } from "react";
 
 type BreadcrumbItemProps = {
-  href: string;
+  href?: string;
   children: ReactNode;
-}
+};
 
-export const BreadcrumbItem: FC<BreadcrumbItemProps> = ({href, children, ...props}) => {
-  return <Link href={href} {...props}>{children}</Link>
-}
+export const BreadcrumbItem: FC<BreadcrumbItemProps> = ({
+  href,
+  children,
+  ...props
+}) => {
+  return href ? (
+    <Link href={href} {...props}>
+      {children}
+    </Link>
+  ) : (
+    <div>{children}</div>
+  );
+};
 
 type BreadcrumbsProps = {
-  children: ReactElement<BreadcrumbItemProps>[] | ReactElement<BreadcrumbItemProps>;
-}
+  children:
+    | ReactElement<BreadcrumbItemProps>[]
+    | ReactElement<BreadcrumbItemProps>;
+};
 
-export const Breadcrumbs: FC<BreadcrumbsProps> = ({children}) => {
-  const childCount = Children.count(children)
+export const Breadcrumbs: FC<BreadcrumbsProps> = ({ children }) => {
+  const childCount = Children.count(children);
   return (
     <div className="flex space-x-1 text-xl font-semibold items-center">
-      {Children.map(children as ReactElement<BreadcrumbItemProps>[], (child, index) => {
-        // You can do something with each child here
-        return <>{child} {index < childCount - 1 && <ChevronRight />}</>
-      })}
+      {Children.map(
+        children as ReactElement<BreadcrumbItemProps>[],
+        (child, index) => {
+          return (
+            <>
+              {child} {index < childCount - 1 && <ChevronRight />}
+            </>
+          );
+        }
+      )}
     </div>
   );
-}
+};
