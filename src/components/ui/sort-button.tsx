47b4684d61cfe655a17abad7bbeb7dc3b42c0733
David Ishmael <dishmael3@gmail.com>
2024-05-10 11:48:18 -0400

    fixed styling

---
+++ src/components/ui/sort-button.tsx
@@ -0,0 +1,32 @@
+import { Column, SortDirection } from "@tanstack/react-table";
+import { Button } from "@nextui-org/button";
+import { ArrowDownIcon, ArrowUpIcon, ChevronsUpDown } from "lucide-react";
+
+type TSortingButton<T> = {
+  title: string;
+  column: Column<T>;
+};
+
+export const SortButton = <T,>({ title, column }: TSortingButton<T>) => {
+  const handleClick = () => {
+    column.toggleSorting();
+  };
+
+  const getIcon = (sort: false | SortDirection) => {
+    switch (sort) {
+      case "desc":
+        return <ArrowDownIcon className="ml-2 h-4 w-4" />;
+      case "asc":
+        return <ArrowUpIcon className="ml-2 h-4 w-4" />;
+      default:
+        return <ChevronsUpDown className="ml-2 h-4 w-4" />;
+    }
+  };
+
+  return (
+    <div onClick={handleClick} className="flex m-0 p-0 cursor-pointer items-center">
+      {title}
+      {getIcon(column.getIsSorted())}
+    </div>
+  );
+};
