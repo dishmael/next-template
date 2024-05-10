681c4979fe2a005641400f4d149ea7f38494a17e
David Ishmael <dishmael3@gmail.com>
2024-05-10 10:52:12 -0400

    Genericized export function

--- src/utils/exportData.ts
+++ src/utils/exportData.ts
@@ -1,15 +1,61 @@
 import { getTimestamp } from ".";
 
 type FileType = "csv" | "json";
-type Exportable = {
-  data: object[];
+type Exportable<T> = {
+  data: T[];
   fileType: FileType;
-  fileName: string;
+  fileName?: string;
+};
+
+/**
+ * exportData converts an array of objects to a format that can be downloaded
+ *
+ * @param data an array of objects
+ * @param fileType the mime-type; csv or json
+ * @param fileName the prefix for the output file name
+ */
+const exportData = <T>({
+  data,
+  fileType,
+  fileName = "data",
+}: Exportable<T>) => {
+  const timestamp = getTimestamp();
+  const outputFileName = `${fileName}-${timestamp}.${fileType}`;
+
+  switch (fileType) {
+    case "csv": {
+      // Convert the data to a CSV string
+      const replacer = (key: string, value: any) =>
+        value === null ? "" : value;
+      const header = Object.keys(data[0] as object);
+      const csvStr = data.map((row) =>
+        header
+          .map((fieldName) =>
+            JSON.stringify((row as { [key: string]: any })[fieldName], replacer)
+          )
+          .join(",")
+      );
+      
+      csvStr.unshift(header.join(","));
+      const csvOutputData = csvStr.join("\r\n");
+      download(csvOutputData, "text/csv", outputFileName);
+      break;
+    }
+
+    case "json": {
+      const jsonOutputData = JSON.stringify(data, null, 2);
+      download(jsonOutputData, "application/json", outputFileName);
+      break;
+    }
+
+    default:
+      throw new Error(`exportData: ${fileType} is not a valid mime-type`);
+  }
 };
 
 /**
  * download takes the exportable data and downloads it to CSV or JSON
- * 
+ *
  * @param data the stringified object array
  * @param type mime-type (e.g. application/json)
  * @param fileName the filename of the downloaded content
@@ -36,50 +82,6 @@ const download = (data: string, type: string, fileName: string) => {
 
   // Remove the anchor from the body
   document.body.removeChild(a);
-};
-
-/**
- * exportData converts an array of objects to a format that can be downloaded
- * 
- * @param data an array of objects
- * @param fileType the mime-type; csv or json
- * @param fileName the prefix for the output file name
- */
-const exportData = ({
-  data,
-  fileType = "json",
-  fileName = "data",
-}: Exportable) => {
-  const outputFileName = `${fileName}-${getTimestamp}.${fileType}`;
-
-  switch (fileType) {
-    case "csv": {
-      // Convert the data to a CSV string
-      const replacer = (key: string, value: any) =>
-        value === null ? "" : value;
-      const header = Object.keys(data[0]);
-      const csvStr = data.map((row) =>
-        header
-          .map((fieldName) =>
-            JSON.stringify((row as { [key: string]: any })[fieldName], replacer)
-          )
-          .join(",")
-      );
-      csvStr.unshift(header.join(","));
-      const csvOutputData = csvStr.join("\r\n");
-      download(csvOutputData, "text/csv", outputFileName);
-      break;
-    }
-
-    case "json": {
-      const jsonOutputData = JSON.stringify(data, null, 2);
-      download(jsonOutputData, "application/json", outputFileName);
-      break;
-    }
-
-    default:
-      throw new Error(`exportData: ${fileType} is not a valid mime-type`)
-  }
 };
 
 export default exportData;
