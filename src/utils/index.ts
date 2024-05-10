7a84527cfcc022ac030d34f24da67f5d3e9322d1
David Ishmael <dishmael3@gmail.com>
2024-05-10 11:47:49 -0400

    fixed imports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,8 @@
-export * from ".";
+import calculateWorkdays from "./calculateWorkDays";
+import cn from "./cn";
+import exportData from "./exportData";
+import getTimestamp from "./getTimestamp";
+
+export * from "./sessionHelpers";
+
+export { calculateWorkdays, cn, exportData, getTimestamp }
